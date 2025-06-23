d997329c8f0b397cd254814bddccccfac61859b7
cjsimm <cjsimm@github.com>
2025-06-23 12:24:42 +0100

    restructured to allow for correct module load in /colors top level dir

---
+++ lua/SeaShells/init.lua
@@ -0,0 +1,10 @@
+local theme = require("theme")
+local config = require("config")
+
+local M = {
+}
+
+function M.load()
+end
+
+return M
--- lua/init.lua
+++
@@ -1,5 +0,0 @@
-local config = require("config")
-
-local M = {}
-
-return M
