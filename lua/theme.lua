e0448e0ac3c68acc00ce675ae5398ebe52f75a1e
cjsimm <cjsimm@github.com>
2025-06-23 12:11:57 +0100

    added style notes

--- lua/theme.lua
+++ lua/theme.lua
@@ -2,6 +2,9 @@ local c = require("colors")
 
 local M = {}
 
+-- Colour palette notes:
+-- red light blue , yellow/orange for highlights and syntax/keywords 
+-- orange for keys in k:v pairs
 function M.setup()
     local theme = {
         highlights = {
