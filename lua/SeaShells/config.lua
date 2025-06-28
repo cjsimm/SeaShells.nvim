01a7695f823f453e3e06c73bbe4bf4f6354f7df8
cjsimm <cjsimm@github.com>
2025-06-28 23:46:44 +0100

    swap to options table as defaults not currently needed

--- lua/SeaShells/config.lua
+++ lua/SeaShells/config.lua
@@ -1,7 +1,9 @@
+-- config file holds any values that can be defined or overridden by the end user
+-- to produce an overall effect or behaviour for the theme, such as transparency
 local M = {}
 
-M.defaults = {
-    transparent = true
+M.options = {
+    transparent = true,
 }
 
 return M
